f843e42d233a0e614b7a6b49eb6810f3628d3fc4
Pavel Pileka <pavelpileka@gmail.com>
2025-10-02 10:00:33 +0300

    Add basic methods
    Add the following to Apartment and PurchaseRequest
    - getters
    - setters
    - toString

--- src/main/java/domain/Apartment.java
+++ src/main/java/domain/Apartment.java
@@ -1,4 +1,57 @@
 package domain;
 
 public class Apartment {
+    private String address;
+    private int roomCount;
+    private int area;
+    private int price; // Stored as integer and divided by 100 for displaying
+
+    public Apartment(String address, int roomCount, int area, int price) {
+        this.address = address;
+        this.roomCount = roomCount;
+        this.area = area;
+        this.price = price;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getRoomCount() {
+        return roomCount;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setRoomCount(int roomCount) {
+        this.roomCount = roomCount;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Address: " + address +
+                "\nRoom count: " + roomCount +
+                "\nArea: " + area +
+                "\nPrice " + price * 1.0 / 100 + "BYN";
+    }
 }
--- src/main/java/domain/PurchaseRequest.java
+++ src/main/java/domain/PurchaseRequest.java
@@ -1,4 +1,68 @@
 package domain;
 
 public class PurchaseRequest {
+    private int roomCount;
+    private int minArea;
+    private int maxArea;
+    private int minPrice;
+    private int maxPrice;
+
+
+    public PurchaseRequest(int roomCount, int minArea, int maxArea, int minPrice, int maxPrice) {
+        this.roomCount = roomCount;
+        this.minArea = minArea;
+        this.maxArea = maxArea;
+        this.minPrice = minPrice;
+        this.maxPrice = maxPrice;
+    }
+
+
+    public int getRoomCount() {
+        return roomCount;
+    }
+
+    public int getMinArea() {
+        return minArea;
+    }
+
+    public int getMaxPrice() {
+        return maxPrice;
+    }
+
+    public int getMinPrice() {
+        return minPrice;
+    }
+
+    public int getMaxArea() {
+        return maxArea;
+    }
+
+
+    public void setMaxArea(int maxArea) {
+        this.maxArea = maxArea;
+    }
+
+    public void setRoomCount(int roomCount) {
+        this.roomCount = roomCount;
+    }
+
+    public void setMinArea(int minArea) {
+        this.minArea = minArea;
+    }
+
+    public void setMinPrice(int minPrice) {
+        this.minPrice = minPrice;
+    }
+
+    public void setMaxPrice(int maxPrice) {
+        this.maxPrice = maxPrice;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Desired room count: " + roomCount +
+                "\nArea range: " + minArea + " - " + maxArea +
+                "\nPrice range: " + minPrice * 1.0 / 100 + " - " + maxPrice * 1.0 / 100 + " BYN";
+    }
 }
