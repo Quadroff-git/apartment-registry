65299af0e1b53ba6591d3f2a5f545dbede13fcd3
Pavel Pileka <pavelpileka@gmail.com>
2025-10-03 20:06:33 +0300

    Add BaseDao abstract class
    Added dao package and BaseDao abstract class for daos to inherit

---
+++ src/main/java/db/dao/BaseDao.java
@@ -0,0 +1,15 @@
+package db.dao;
+
+import java.sql.Connection;
+import java.util.List;
+
+public abstract class BaseDao<T> {
+    private Connection connection;
+
+    public abstract List<T> getAll();
+    public abstract T findById(int id);
+    public abstract boolean delete(int id);
+    public abstract boolean delete(T entity);
+    public abstract boolean create(T entity);
+    public abstract T update(T entity);
+}
