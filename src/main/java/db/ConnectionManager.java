bda82b210c680c77f9e72ca4f17ba1e3dee10bb3
Pavel Pileka <pavelpileka@gmail.com>
2025-10-03 19:47:41 +0300

    Add ConnectionManager class
    * Added postgres jdbc driver gradle dependency
    * Added a simple wrapper class for managing the db connection

---
+++ src/main/java/db/ConnectionManager.java
@@ -0,0 +1,21 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    private Connection connection;
+
+    public ConnectionManager(String url, String login, String password) throws SQLException {
+        this.connection = DriverManager.getConnection(url, login, password);
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection() throws SQLException {
+        connection.close();
+    }
+}
