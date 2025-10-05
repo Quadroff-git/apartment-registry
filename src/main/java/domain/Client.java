37a78b1427feee2a70b389d5d7dd80b914c51916
Pavel Pileka <pavelpileka@gmail.com>
2025-10-05 16:12:17 +0300

    Add id field to Client

--- src/main/java/domain/Client.java
+++ src/main/java/domain/Client.java
@@ -1,6 +1,7 @@
 package domain;
 
 public class Client {
+    private int id;
     private String fullName;
     private String phoneNumber;
 
@@ -10,6 +11,11 @@ public class Client {
         this.phoneNumber = phoneNumber;
     }
 
+    public Client(int id, String fullName, String phoneNumber) {
+        this.id = id;
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+    }
 
     public String getFullName() {
         return fullName;
