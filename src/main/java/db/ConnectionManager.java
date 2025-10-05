b6b9289265602dc633faa1c9853b7b9e9798ed27
Pavel Pileka <pavelpileka@gmail.com>
2025-10-05 17:10:09 +0300

    Made connection field protected in BaseDao

--- src/main/java/db/ConnectionManager.java
+++ src/main/java/db/ConnectionManager.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionManager {
-    private Connection connection;
+    protected Connection connection;
 
     public ConnectionManager(String url, String login, String password) throws SQLException {
         this.connection = DriverManager.getConnection(url, login, password);
@@ -18,4 +18,16 @@ public class ConnectionManager {
     public void closeConnection() throws SQLException {
         connection.close();
     }
+
+
+    public static void main(String[] argv) {
+        try {
+            ConnectionManager conn = new ConnectionManager(argv[0], argv[1], argv[2]);
+            System.out.println(conn.getConnection().isValid(0));
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
 }
+
