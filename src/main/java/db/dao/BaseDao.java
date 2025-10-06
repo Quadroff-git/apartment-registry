53ec7384d05553d2a69295c75fb7b4b0f4ae59a3
Pavel Pileka <pavelpileka@gmail.com>
2025-10-06 19:04:09 +0300

    Add constructor to BaseDao

--- src/main/java/db/dao/BaseDao.java
+++ src/main/java/db/dao/BaseDao.java
@@ -6,6 +6,10 @@ import java.util.List;
 public abstract class BaseDao<T> {
     private Connection connection;
 
+    public BaseDao(Connection connection) {
+        this.connection = connection;
+    }
+
     public abstract List<T> getAll();
     public abstract T findById(int id);
     public abstract boolean delete(int id);
