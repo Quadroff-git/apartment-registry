8c814ea5ea7d388ce94d6c8116a1a7beb7972d7a
Pavel Pileka <pavelpileka@gmail.com>
2025-10-08 12:19:05 +0300

    Split address field into street, building and number

--- src/main/java/domain/Apartment.java
+++ src/main/java/domain/Apartment.java
@@ -2,33 +2,44 @@ package domain;
 
 public class Apartment {
     private Integer id;
-    private String address;
+    private String street;
+    private int building;
+    private int number;
     private int roomCount;
     private int area;
     private int price; // Stored as integer and divided by 100 for displaying
 
 
-    public Apartment(String address, int roomCount, int area, int price) {
+    public Apartment(String street, int building, int number, int roomCount, int area, int price) {
         this.id = null;
-        this.address = address;
+        this.street = street;
+        this.building = building;
+        this.number = number;
         this.roomCount = roomCount;
         this.area = area;
         this.price = price;
     }
 
-    public Apartment(int id, String address, int roomCount, int area, int price) {
+    public Apartment(int id, String street, int building, int number, int roomCount, int area, int price) {
+        this(street, building, number, roomCount, area, price);
         this.id = id;
-        this.address = address;
-        this.roomCount = roomCount;
-        this.area = area;
-        this.price = price;
     }
 
 
-    public Integer getId() { return id; }
+    public Integer getId() {
+        return id;
+    }
 
-    public String getAddress() {
-        return address;
+    public String getStreet() {
+        return street;
+    }
+
+    public int getBuilding() {
+        return building;
+    }
+
+    public int getNumber() {
+        return number;
     }
 
     public int getRoomCount() {
@@ -44,10 +55,20 @@ public class Apartment {
     }
 
 
-    public void setId(Integer id) { this.id = id; }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setBuilding(int building) {
+        this.building = building;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
     }
 
     public void setRoomCount(int roomCount) {
@@ -65,7 +86,7 @@ public class Apartment {
 
     @Override
     public String toString() {
-        return "Address: " + address +
+        return "Address: " + street + " " + building + ", " + number +
                 "\nRoom count: " + roomCount +
                 "\nArea: " + area +
                 "\nPrice " + price * 1.0 / 100 + "BYN";
