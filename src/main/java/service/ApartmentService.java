2200910b8dd731688b66277af97b4a1391272d2b
Pavel Pileka <pavelpileka@gmail.com>
2025-10-16 20:59:05 +0300

    Add ApartmentService class

---
+++ src/main/java/service/ApartmentService.java
@@ -0,0 +1,33 @@
+package service;
+
+import domain.Apartment;
+
+import java.util.List;
+
+public class ApartmentService extends BaseService<Apartment> {
+
+    @Override
+    public List<Apartment> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public Apartment getById(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean create(Apartment entity) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Apartment entity) {
+        return false;
+    }
+}
