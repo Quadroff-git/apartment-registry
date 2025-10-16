9086498cd8e561f0d650372bd797cdc2c9fc5571
Pavel Pileka <pavelpileka@gmail.com>
2025-10-16 22:00:33 +0300

    Override BaseService methods in ApartmentService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import db.ConnectionManager;
 import db.dao.ClientDao;
 import db.dao.PurchaseRequestDao;
+import db.dao.TransactionManager;
 import domain.Apartment;
 import domain.Client;
 import domain.PurchaseRequest;
+import service.ApartmentService;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -11,16 +13,9 @@ import java.util.List;
 public class Main {
     public static void main(String[] argv) {
         try {
-            ConnectionManager connectionManager = new ConnectionManager(argv[0], argv[1], argv[2]);
-            PurchaseRequestDao purchaseRequestDao = new PurchaseRequestDao(connectionManager.getConnection());
-
-            Apartment a = new Apartment("", 1, 1, 2, 72, 8000000);
-
-            List<PurchaseRequest> pr = purchaseRequestDao.findByApartment(a);
-            for (PurchaseRequest p : pr) {
-                System.out.println(p + "\n");
-            }
-
+            ConnectionManager cm = new ConnectionManager(argv[0], argv[1], argv[2]);
+            ApartmentService as = new ApartmentService(cm);
+            System.out.println(as.getById(16));
 
 
         } catch (SQLException e) {
--- src/main/java/service/ApartmentService.java
+++ src/main/java/service/ApartmentService.java
@@ -1,8 +1,10 @@
 package service;
 
 import db.ConnectionManager;
+import db.dao.ApartmentDao;
 import domain.Apartment;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class ApartmentService extends BaseService<Apartment> {
@@ -13,27 +15,74 @@ public class ApartmentService extends BaseService<Apartment> {
 
 
     @Override
-    public List<Apartment> getAll() {
-        return List.of();
+    public List<Apartment> getAll() throws SQLException {
+        ApartmentDao apartmentDao = new ApartmentDao();
+        transactionManager.initTransaction(apartmentDao);
+
+        List<Apartment> apartments = apartmentDao.getAll();
+
+        transactionManager.endTransaction();
+
+        return apartments;
     }
 
     @Override
-    public Apartment getById(int id) {
-        return null;
+    public Apartment getById(int id) throws SQLException {
+        if (id < 0) {
+            throw new IllegalArgumentException("id can't be negative");
+        }
+
+        ApartmentDao apartmentDao = new ApartmentDao();
+        transactionManager.initTransaction(apartmentDao);
+
+        Apartment apartment = apartmentDao.findById(id);
+
+        transactionManager.endTransaction();
+        return apartment;
     }
 
     @Override
-    public boolean create(Apartment entity) {
-        return false;
+    public boolean create(Apartment entity) throws SQLException{
+        if (entity == null) {
+            throw new IllegalArgumentException("Apartment passed as argument is null");
+        }
+
+        ApartmentDao apartmentDao = new ApartmentDao();
+        transactionManager.initTransaction(apartmentDao);
+
+        boolean res = apartmentDao.create(entity);
+
+        transactionManager.endTransaction();
+        return res;
     }
 
     @Override
-    public boolean delete(int id) {
-        return false;
+    public boolean delete(int id) throws SQLException{
+        if (id < 0) {
+            throw new IllegalArgumentException("id can't be negative");
+        }
+
+        ApartmentDao apartmentDao = new ApartmentDao();
+        transactionManager.initTransaction(apartmentDao);
+
+        boolean res = apartmentDao.delete(id);
+
+        transactionManager.endTransaction();
+        return res;
     }
 
     @Override
-    public boolean update(Apartment entity) {
-        return false;
+    public boolean update(Apartment entity) throws SQLException{
+        if (entity == null) {
+            throw new IllegalArgumentException("Apartment passed as argument is null");
+        }
+
+        ApartmentDao apartmentDao = new ApartmentDao();
+        transactionManager.initTransaction(apartmentDao);
+
+        boolean res = (apartmentDao.update(entity) != null);
+
+        transactionManager.endTransaction();
+        return res;
     }
 }
