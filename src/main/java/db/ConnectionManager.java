49be765d6f29c1ea6380da3aafc22abe46f00824
Pavel Pileka <pavelpileka@gmail.com>
2025-10-16 20:59:56 +0300

    Change ConnectionManager to only issue new connections

--- src/main/java/db/ConnectionManager.java
+++ src/main/java/db/ConnectionManager.java
@@ -5,18 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionManager {
-    protected Connection connection;
+    private String url;
+    private String login;
+    private String password;
 
-    public ConnectionManager(String url, String login, String password) throws SQLException {
-        this.connection = DriverManager.getConnection(url, login, password);
+    public ConnectionManager(String url, String login, String password) {
+        this.url = url;
+        this.login = login;
+        this.password = password;
     }
 
-    public Connection getConnection() {
-        return connection;
-    }
-
-    public void closeConnection() throws SQLException {
-        connection.close();
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, login, password);
     }
 }
 
