0d140aef046b848589893cbe79faebabc2c1279c
Pavel Pileka <pavelpileka@gmail.com>
2025-10-19 11:38:15 +0300

    Change TransactionManager to not throw checked SQLException

--- src/main/java/db/TransactionManager.java
+++ src/main/java/db/TransactionManager.java
@@ -14,28 +14,44 @@ public class TransactionManager {
         this.connection = null;
     }
 
-    public void initTransaction(BaseDao dao, BaseDao... daos) throws SQLException {
-        if (connection == null) {
-            connection = connectionManager.getConnection();
-        }
-        connection.setAutoCommit(false);
+    public void initTransaction(BaseDao dao, BaseDao... daos) {
+        try {
+            if (connection == null) {
+                connection = connectionManager.getConnection();
+            }
+            connection.setAutoCommit(false);
 
-        dao.setConnection(connection);
-        for (BaseDao d : daos) {
-            d.setConnection(connection);
+            dao.setConnection(connection);
+            for (BaseDao d : daos) {
+                d.setConnection(connection);
+            }
+        } catch (SQLException e) {
+            System.out.println("ERROR: Connection initialization failed");
         }
     }
 
-    public void endTransaction() throws SQLException{
-        connection.close();
+    public void endTransaction() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("ERROR: Connection closure failed");
+        }
         connection = null;
     }
 
-    public void commit() throws SQLException {
-        connection.commit();
+    public void commit() {
+        try {
+            connection.commit();
+        } catch (SQLException e) {
+            System.out.println("ERROR: Connection commit failed");
+        }
     }
 
-    public void rollback() throws SQLException {
-        connection.rollback();
+    public void rollback() {
+        try {
+            connection.rollback();
+        } catch (SQLException e) {
+            System.out.println("ERROR: Connection rollback failed");
+        }
     }
 }
