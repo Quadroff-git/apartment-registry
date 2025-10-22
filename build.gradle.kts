d8b5a2b8a7b2f7d6c461bd40625ceec597c5fe86
Pavel Pileka <pavelpileka@gmail.com>
2025-10-22 17:32:25 +0300

    Configure build
    - Much gradle fuckery
    - Removed unneeded commits from Main.java

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,42 @@
 plugins {
-    id("java")
+    id ("application")
 }
 
-group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+application {
+    mainClass = "Main"
+}
+
+tasks.withType<CreateStartScripts> {
+    doLast {
+        // Modify Windows batch script
+        val windowsScriptFile = File(outputDir, "${applicationName}.bat")
+        var windowsScript = windowsScriptFile.readText()
+
+        // Add DEFAULT_ARGS variable right after the @rem section
+        windowsScript = windowsScript.replace(
+            "@rem Set local scope for the variables with windows NT shell",
+            """@rem Set local scope for the variables with windows NT shell
+
+@rem Command line arguments for db connection
+@rem Format: url user password
+set DEFAULT_ARGS=jdbc:postgresql:apartment_registry postgres root
+"""
+        )
+
+        // Use the DEFAULT_ARGS in the execution line
+        windowsScript = windowsScript.replace(
+            "-classpath \"%CLASSPATH%\" Main %",
+            "-classpath \"%CLASSPATH%\" Main %DEFAULT_ARGS% %"
+        )
+
+        windowsScriptFile.writeText(windowsScript)
+    }
 }
 
 dependencies {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,5 @@
 import db.ConnectionManager;
-import domain.Apartment;
-import domain.PurchaseRequest;
 import presentation.GUI;
-import service.ApartmentService;
-import service.PurchaseRequestService;
-
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] argv) {
@@ -13,7 +7,6 @@ public class Main {
             ConnectionManager cm = new ConnectionManager(argv[0], argv[1], argv[2]);
             GUI gui = new GUI(cm);
             gui.start();
-
         } catch (Exception e) {
             System.out.println(e);
         }
