1a801bb011213cddb9e4f9a8bec5d528320ec79e
Pavel Pileka <pavelpileka@gmail.com>
2025-11-08 15:50:22 +0300

    Implement doPost in ApartmentServlet

--- src/main/java/web/servlet/ApartmentServlet.java
+++ src/main/java/web/servlet/ApartmentServlet.java
@@ -1,10 +1,12 @@
 package web.servlet;
 
 import domain.Apartment;
+import domain.PurchaseRequest;
 import service.ApartmentService;
 import service.ServiceException;
 import tools.jackson.jr.ob.JSON;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @WebServlet("/apartment")
 public class ApartmentServlet extends HttpServlet {
@@ -30,6 +33,29 @@ public class ApartmentServlet extends HttpServlet {
             response.setCharacterEncoding("UTF-8");
             PrintWriter writer = response.getWriter();
             JSON.std.write(apartments, writer);
+        } catch (ServiceException e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write(JSON.std.asString(e));
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Apartment apartment = JSON.std.beanFrom(Apartment.class, request.getReader());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        List<PurchaseRequest> purchaseRequests = null;
+        try {
+             purchaseRequests = apartmentService.createWithCheck(apartment);
+
+            if (purchaseRequests.isEmpty()) {
+                response.setStatus(HttpServletResponse.SC_CREATED);
+            }
+            else {
+                response.setStatus(HttpServletResponse.SC_OK);
+                JSON.std.write(purchaseRequests, response.getWriter());
+            }
         } catch (ServiceException e) {
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             response.getWriter().write(JSON.std.asString(e));
